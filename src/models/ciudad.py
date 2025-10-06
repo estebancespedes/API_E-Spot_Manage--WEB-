da67ff0b2271aa1222d4b75992834716420cd362
estebancespedes <estebancespedesg@gmail.com>
2025-10-05 22:16:14 -0500

    Update: Se agrego el docstring del modelo ciudad

--- src/models/ciudad.py
+++ src/models/ciudad.py
@@ -9,6 +9,15 @@ from src.database.base_class import Base
 
 
 class ciudad(Base):
+    """
+    Tabla ciudad:
+        Usada como directorio de ciudades
+        Atributos
+            id_ciudad : UUID
+            id_pais : UUID
+            nombre : varchar(50)
+            descripcion : TEXT
+    """
     id_ciudad = Column(UUID, primary_key=False, default=uuid4())
     id_pais = Column(UUID, ForeignKey("region.id_region"), nullable=False)
     nombre = Column(VARCHAR(50), nullable=False)
