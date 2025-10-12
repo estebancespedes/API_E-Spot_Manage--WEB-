5b95a84e4dbb4a6d358e1d20fc575e532c8990ae
estebancespedes <estebancespedesg@gmail.com>
2025-10-11 19:22:57 -0500

    Feat: Modelado completo de la tabla roles

---
+++ src/models/rol.py
@@ -0,0 +1,21 @@
+# importaciones de librerias
+from sqlalchemy import Column, UUID, VARCHAR, TEXT, ForeignKey
+from uuid import uuid4
+
+from sqlalchemy.orm import relationship
+
+# importación del modelo base
+from src.database.base_class import Base
+
+class rol(Base):
+    """
+    Tabla roles
+        atributos:
+        id_rol: uuid
+        nombre: varchar(50)
+    """
+    id_rol = Column(UUID, primary_key=True, default=uuid4())
+    nombre = Column(VARCHAR(50), nullable=False)
+
+    # relaciones
+    usuarios = relationship('usuario', back_populates='rol')
